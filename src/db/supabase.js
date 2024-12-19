2cbf2783c32a51bcafb844ad873221046edca5c5
Harsh verma <hv72002@gmail.com>
2024-12-19 10:31:34 +0530

    Auth issue is fixed

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export { supabaseUrl };
 export default supabase
-        
